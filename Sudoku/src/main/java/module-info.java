ed1d7500ee3568c70db9276762137c3c7e87c56a
Filio1 <103116949+Filio1@users.noreply.github.com>
2022-12-21 00:04:55 +0200

    Delete Sudoku directory

--- Sudoku/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module sudoku.sudoku {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.desktop;
-
-
-    opens sudoku.sudoku to javafx.fxml;
-    exports sudoku.sudoku;
-}
